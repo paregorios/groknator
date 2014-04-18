49db46f0571378e42ef949a0a7019d5b1a33b92f
Tom Elliott <tom.elliott@nyu.edu>
2014-04-18 10:06:30 -0500

    and so it begins

---
+++ README.md
@@ -0,0 +1,4 @@
+Groknator
+=========
+
+What is this file really? What does it contain? What can I do with it? Gather the relevant grokrrrs and unleash the groknator.
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools.import setup
+setup(name='Groknator',
+      version='0.1',
+      author='Tom Elliott',
+      author_email='tom.elliott@nyu.edu',
+      license='BSD-3-clause',
+      description='Throw content at a Groknator, it will try to grok it and tell you what it groks.',
+      long_description=readme,
+      packages=find_packages())
