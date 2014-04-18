fee36f9067e1396b12f7e021db1c73a492a59ee2
Tom Elliott <tom.elliott@nyu.edu>
2014-04-18 11:43:00 -0500

    successfully parsing elements

--- grokrrrs/html.py
+++ grokrrrs/html.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from base import BaseGrokrrr
+from bs4 import BeautifulSoup
 from http import HTTPGrokrrr
 import logging
+import re
 import urllib2
 
 HTMLMIMETYPES = [
@@ -41,23 +43,72 @@ class HTMLGrokrrr(BaseGrokrrr, HTTPGrokrrr):
     def grok(self):
         logger = self.loggers['html']
         try:
-            mimetype = self.http['response_headers']['content-type']
+            contenttype = self.http['response_headers']['content-type']
         except KeyError:
             logger.warning('response headers did not indicate a mimetype, so assuming text/html')
             mimetype = 'text/html'
+        else:
+            if ';' in contenttype:
+                mimetype, charset = contenttype.split(';')
+            else:
+                mimetype = contenttype.strip()
         if mimetype in HTMLMIMETYPES:
             logger.debug('treating content as %s' % mimetype)
-            soup = BeautifulSoup(self.content)
+            soup = BeautifulSoup(self.rawcontent)
+            self.html['soup'] = soup
         else:
             logger.error('content was not grokked because there is no handler for mimetype %s' % mimetype)
             return
 
         # index elements
+        ei = ElementIndexer()
+        self.html['elements'] = ei.index(soup)
         # index attributes
         # index words
         # index meta tags
         # index schema.org microdata
         # index rdfa microdata
         # identify alternate formats referenced in links
+        # what is @rel attribute and how to capture?
+
+class HTMLIndexer:
+    """
+    basic indexer class ClassName(object):
+    """
+
+    def __init__(self):
+        pass
+
+class ElementIndexer(HTMLIndexer):
+    """
+    foo
+    """
+
+    def __init__(self):
+        self.logger = logging.getLogger('groknator.grokrrrs.html.ElementIndexer')
+        HTMLIndexer.__init__(self)
+
+    def index(self, soup):
+        logger = self.logger
+        elements = {}
+        tagnames = set([t.name.strip() for t in soup.find_all(True)])
+        for tagname in tagnames:
+            elements[tagname] = {}
+            tags = soup.find_all(tagname)
+            elements[tagname]['count'] = len(tags)
+            elements[tagname]['instances'] = []
+            for tag in tags:
+                ele = {}
+                ele['attributes'] = []
+                for a in sorted(tag.attrs.keys()):
+                    val = tag.attrs[a]
+                    if type(val) is list:
+                        val = u' '.join(val)
+                    p = re.compile(u'\s+')
+                    val = p.sub(u' ', val).strip()
+                    ele['attributes'].append({a:val})
+                elements[tagname]['instances'].append(ele)
+        logger.debug("elements: %s" % sorted(elements.keys()))
+        return elements
 
 
