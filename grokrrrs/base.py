a365d3f87ac1453480f90ac394693fd2479d6d70
Tom Elliott <tom.elliott@nyu.edu>
2014-04-18 10:09:04 -0500

    skeleton for base class

---
+++ grokrrrs/base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class BaseGrokrrr:
+    """
+    Base grokkr class: everything a baby grokkr needs to know
+    """
+
+    def __init__(self):
+        pass
