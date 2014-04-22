b236dc5d61b24ae5ed9048a948e7c6633a682f4a
Tom Elliott <tom.elliott@nyu.edu>
2014-04-22 10:47:14 -0500

    get rid of long description for now and actually import find_packages so it works

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools.import setup
+from setuptools import setup, find_packages
 setup(name='Groknator',
       version='0.1',
       author='Tom Elliott',
       author_email='tom.elliott@nyu.edu',
       license='BSD-3-clause',
       description='Throw content at a Groknator, it will try to grok it and tell you what it groks.',
-      long_description=readme,
       packages=find_packages())
